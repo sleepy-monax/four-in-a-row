132163613742606ac28e46ea7bc97fe9147e3136
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2020-02-08 18:47:15 +0100

    Project: Configured the project to build under Linux.

--- src/utils/Main.java
+++ src/utils/Main.java
@@ -1,20 +1,14 @@
-package sample;
+package utils;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
     @Override
     public void start(Stage primaryStage) throws Exception{
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene();
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
