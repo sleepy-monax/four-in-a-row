274a68c139d6dfc28f07b076617b76c345afa308
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2020-02-18 13:37:46 +0100

    Views: Improved layout of the main menu.

--- pGroupeAB02/src/views/MainMenu.java
+++ pGroupeAB02/src/views/MainMenu.java
@@ -2,29 +2,23 @@ package views;
 
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.layout.*;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import utils.Main;
 
 public class MainMenu extends StackPane {
-    public MainMenu()
-    {
-        Pane singleplayerButton = Widgets.makeBigButton("assets/singleplayer.png", "Singleplayer" );
+    public MainMenu() {
+        Pane singleplayerButton = Widgets.makeBigButton("assets/singleplayer.png", "Singleplayer");
         singleplayerButton.setPadding(new Insets(0, 72, 0, 72));
 
-        Pane multiplayerButton = Widgets.makeBigButton( "assets/multiplayer.png","Multiplayer");
+        Pane multiplayerButton = Widgets.makeBigButton("assets/multiplayer.png", "Multiplayer");
         multiplayerButton.setPadding(new Insets(0, 72, 0, 72));
 
         Pane orbScores = Widgets.makeOrbButton("assets/score.png");
-        Pane orbSettings= Widgets.makeOrbButton("assets/settings.png");
-        Pane orbQuit= Widgets.makeOrbButton("assets/close.png");
-
-        VBox menuContainer = new VBox(16);
-        menuContainer.setAlignment(Pos.CENTER);
-        menuContainer.getChildren().addAll(
-                Widgets.makeLogo(),
-                singleplayerButton,
-                multiplayerButton);
-        menuContainer.setMaxWidth(436);
+        Pane orbSettings = Widgets.makeOrbButton("assets/settings.png");
+        Pane orbQuit = Widgets.makeOrbButton("assets/close.png");
 
         HBox orbContainer = new HBox(16, orbScores, orbSettings, orbQuit);
         orbContainer.setAlignment(Pos.CENTER);
@@ -32,10 +26,20 @@ public class MainMenu extends StackPane {
         orbContainer.setMaxHeight(48);
         StackPane.setAlignment(orbContainer, Pos.BOTTOM_CENTER);
 
+        VBox menuContainer = new VBox(
+                16,
+                Widgets.makeLogo(),
+                singleplayerButton,
+                multiplayerButton,
+                orbContainer
+        ) {{
+            setAlignment(Pos.CENTER);
+            setMaxWidth(512);
+        }};
+
         this.setAlignment(Pos.CENTER);
         this.setId("background");
-        this.setPadding(new Insets(32));
-        this.getChildren().addAll(menuContainer ,orbContainer);
+        this.getChildren().add(menuContainer);
 
         multiplayerButton.setOnMouseClicked(mouseEvent -> Main.switchScene(new MultiplayerSelect()));
         orbQuit.setOnMouseClicked(mouseEvent -> Main.quit());
