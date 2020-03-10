13bde03a6256e4d24a54c98e5ec93ac86e629384
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2020-03-10 19:33:06 +0100

    Editor+Score: Centered the table view.

--- pGroupeAB02/src/views/Editor.java
+++ pGroupeAB02/src/views/Editor.java
@@ -1,12 +1,23 @@
 package views;
 
+import controls.Title;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.layout.StackPane;
 import models.Deck;
+import utils.StageManager;
 
 public class Editor extends View {
     public Editor(Deck deck) {
+        setPadding(new Insets(32));
+
         TableView table = new TableView();
+
+        table.setMaxWidth(512);
+        table.setMaxHeight(480);
 
         TableColumn answerColumn = new TableColumn("Answer");
         TableColumn themeColumn = new TableColumn("Theme");
@@ -15,6 +26,10 @@ public class Editor extends View {
 
         table.getColumns().addAll(answerColumn, themeColumn, authorColumn, cluesColumn);
 
-        getChildren().add(table);
+        Button backButton = Widgets.makeButton("Go back");
+        backButton.setOnAction(actionEvent -> StageManager.switchView(new MainMenu()));
+        StackPane.setAlignment(backButton, Pos.BOTTOM_LEFT);
+
+        getChildren().addAll(new Title("Editor"), table, backButton);
     }
 }
--- pGroupeAB02/src/views/Score.java
+++ pGroupeAB02/src/views/Score.java
@@ -1,33 +1,32 @@
 package views;
 
 import controls.Title;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
 import models.Player;
 import utils.StageManager;
 
 public class Score extends View {
     TableView tableView = new TableView();
-    public Score(){
 
-
-
+    public Score() {
+        setPadding(new Insets(32));
+        tableView.setMaxWidth(512);
+        tableView.setMaxHeight(480);
 
         TableColumn<String, Player> column1 = new TableColumn<>("Position");
         column1.setCellValueFactory(new PropertyValueFactory<>("position"));
 
-
         TableColumn<String, Player> column2 = new TableColumn<>("Name");
         column2.setCellValueFactory(new PropertyValueFactory<>("name"));
 
-        TableColumn<String, Player> column3= new TableColumn<>("Score");
+        TableColumn<String, Player> column3 = new TableColumn<>("Score");
         column3.setCellValueFactory(new PropertyValueFactory<>("score"));
-
 
         tableView.getColumns().add(column1);
         tableView.getColumns().add(column2);
@@ -37,12 +36,6 @@ public class Score extends View {
         backButton.setOnAction(actionEvent -> StageManager.switchView(new MainMenu()));
         StackPane.setAlignment(backButton, Pos.BOTTOM_LEFT);
 
-
-        VBox vbox = new VBox(tableView);
-
         this.getChildren().addAll(new Title("Score"), tableView, backButton);
-
-
-
     }
 }
