7fee75cfbc1763fbf245429ea29e73e822d829d1
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2020-03-30 10:56:54 +0200

    Audio: Cleanup OpenAL.java

--- pGroupeAB02/src/audio/OpenAL.java
+++ pGroupeAB02/src/audio/OpenAL.java
@@ -1,58 +1,45 @@
 package audio;
 
 import org.lwjgl.openal.*;
-import org.lwjgl.system.*;
 
-import java.nio.*;
-import static org.lwjgl.stb.STBVorbis.*;
 import static org.lwjgl.openal.AL10.*;
 import static org.lwjgl.openal.ALC10.*;
-import static org.lwjgl.system.MemoryStack.*;
-import static org.lwjgl.system.libc.LibCStdlib.*;
 
 public class OpenAL {
-    public static void main(String[] args)
-    {
-        //Initialization
+    public static void main(String[] args) {
+        // Initialization
         String defaultDeviceName = alcGetString(0, ALC_DEFAULT_DEVICE_SPECIFIER);
-        long   device            = alcOpenDevice(defaultDeviceName);
+        long device = alcOpenDevice(defaultDeviceName);
 
-        int[] attributes = {0};
-        long  context    = alcCreateContext(device, attributes);
+        int[] attributes = { 0 };
+        long context = alcCreateContext(device, attributes);
         alcMakeContextCurrent(context);
 
         ALCCapabilities alcCapabilities = ALC.createCapabilities(device);
-        ALCapabilities  alCapabilities  = AL.createCapabilities(alcCapabilities);
+        ALCapabilities alCapabilities = AL.createCapabilities(alcCapabilities);
 
         AudioBuffer buffer = AudioBuffer.loadFromFile("src/assets/musics/loop.ogg");
-        AudioBuffer buffer2 = AudioBuffer.loadFromFile("src/assets/musics/loop3.ogg");
+        AudioBuffer buffer2 = AudioBuffer.loadFromFile("src/assets/musics/loop2.ogg");
 
-//Request a source
+        // Request a source
         int sourcePointer = alGenSources();
 
-//Assign the sound we just loaded to the source
+        // Assign the sound we just loaded to the source
         alSourcei(sourcePointer, AL_LOOPING, 1);
 
-//Play the sound
-
+        // Play the sound
+        alSourceQueueBuffers(sourcePointer, buffer.handle());
+        alSourceQueueBuffers(sourcePointer, buffer2.handle());
+        alSourcePlay(sourcePointer);
 
         try {
-            //Wait for a second
-            alSourcei(sourcePointer, AL_BUFFER, buffer2.handle());
-
-            alSourcePlay(sourcePointer);
-
-
-            Thread.sleep(1000);
-
-            alSourcei(sourcePointer, AL_BUFFER, buffer2.handle());
-
+            // Wait for a second
 
             Thread.sleep(1000000);
         } catch (InterruptedException ignored) {
         }
 
-//Terminate OpenAL
+        // Terminate OpenAL
         alDeleteSources(sourcePointer);
         buffer.close();
         alcDestroyContext(context);
