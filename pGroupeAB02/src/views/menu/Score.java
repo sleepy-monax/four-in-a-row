7ea0107112d1072e27e03fe0b0d1bafc6d453361
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2020-05-04 01:24:37 +0200

    Scores: Added positions.

--- pGroupeAB02/src/views/menu/Score.java
+++ pGroupeAB02/src/views/menu/Score.java
@@ -1,11 +1,15 @@
 package views.menu;
 
+import javafx.beans.property.ReadOnlyStringWrapper;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TableColumn.CellDataFeatures;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.Region;
+import javafx.util.Callback;
 import models.Player;
 import models.scores.ListScore;
 import utils.StageManager;
@@ -15,6 +19,8 @@ import views.View;
 import static views.Layout.*;
 import static views.Widget.*;
 
+import java.util.Comparator;
+
 public class Score extends View {
     public Score() {
         super(true);
@@ -22,16 +28,33 @@ public class Score extends View {
         TableView table = new TableView();
 
         ObservableList<models.scores.Score> data = FXCollections.observableArrayList(ListScore.get().getScores());
+
+
+        Comparator< models.scores.Score> comparator = Comparator.comparingInt(models.scores.Score::getScore); 
+        FXCollections.sort(data, comparator);
+        FXCollections.reverse(data);
+        
+
         table.setItems(data);
 
-        TableColumn<String, Player> column1 = new TableColumn<>("Name");
-        column1.setCellValueFactory(new PropertyValueFactory<>("name"));
+        TableColumn<Score, String> column1 = new TableColumn<>("Place");
+        column1.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<Score,String>,ObservableValue<String>>(){
+        
+                @Override
+                public ObservableValue<String> call(CellDataFeatures<Score, String> param) {
+                        return  new ReadOnlyStringWrapper("" + (data.indexOf(param.getValue()) + 1)) ;
+                }
+        });
 
-        TableColumn<String, Player> column2 = new TableColumn<>("Score");
-        column2.setCellValueFactory(new PropertyValueFactory<>("score"));
+        TableColumn<String, Player> column2 = new TableColumn<>("Name");
+        column2.setCellValueFactory(new PropertyValueFactory<>("name"));
+
+        TableColumn<String, Player> column3 = new TableColumn<>("Score");
+        column3.setCellValueFactory(new PropertyValueFactory<>("score"));
 
         table.getColumns().add(column1);
         table.getColumns().add(column2);
+        table.getColumns().add(column3);
 
         Region scorePanel = panel(
                 vertical(
